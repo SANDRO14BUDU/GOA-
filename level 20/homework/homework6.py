04cffee38d570dfd4ad114eb8a5a48003dd225a6
qweqwe1qq <qweqwe@gmail.com>
2024-12-15 13:09:11 +0400

    commit

--- level 20/homework/homework6.py
+++ level 20/homework/homework6.py
@@ -1,13 +1,17 @@
 # 7) Write a program that checks if a single given number is prime number
 
-number = int(input("Enter a number: "))
+# number = int(input("Enter a number: "))
 
-if number > 1:
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            print(f"{number} is not a prime number.")
-            break
-    else:
-        print(f"{number} is a prime number.")
-else:
-    print(f"{number} is not a prime number.")
+# if number > 1:
+#     for i in range(2, int(number ** 0.5) + 1):
+#         if number % i == 0:
+#             print(f"{number} is not a prime number.")
+#             break
+#     else:
+#         print(f"{number} is a prime number.")
+# else:
+#     print(f"{number} is not a prime number.")
+
+
+for i in range(1, 100):
+    print(f'{i} fr')
